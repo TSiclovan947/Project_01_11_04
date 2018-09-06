70e02e72ee49917da52c332b5af508f77d7a253e
Unknown <tsiclovan947@west-mec.org>
2018-09-05 17:21:04 -0700

    Final Code Commit
    Created a function to implement radio buttons; Created a function to get data back with displayData; Created an event listener for checkButtons click event; Changed the css and added an html fieldset

--- script.js
+++ script.js
@@ -9,10 +9,25 @@
 "use strict";
 
 var httpRequest = false;
+var countrySel; //track the selected country
 
-//
+//Function to implement the radio buttons
+function checkButtons() {
+    var germany = document.getElementById("germany");
+    var us = document.getElementById("us");
+    if (germany.checked || us.checked) {
+        document.getElementById("zipset").style.visibility = "visible";
+        if (germany.checked) {
+            countrySel = "de";
+        }
+        else {
+            countrySel = "us";
+        }
+    }
+}
+
+//Function to check the length of the zip code
 function checkInput() {
-    //alert("checkInput()");
     var zip = document.getElementById("zip").value;
     if (zip.length === 5) {
         getLocation();
@@ -23,23 +38,28 @@ function checkInput() {
     }
 }
 
-//Function to
+//Function to create an XHR object
 function getRequestObject() {
     try {
         httpRequest = new XMLHttpRequest();
     }
     catch (requestError) {
+        document.getElementById("zipset").style.visibility = "visible";
         document.getElementById("csset").style.visibility = "visible";
+        var germany = document.getElementById("germany");
+        var us = document.getElementById("us");
         var zip = document.getElementById("zip").value;
         if (zip.addEventListener) {
+            germany.removeEventListener("click", checkButtons, false);
+            us.removeEventListener("click", checkButtons, false);
             zip.removeEventListener("keyup", checkInput, false);
         }
         else if (zip.attachEvent) {
+            germany.detachEvent("onclick", checkButtons);
             zip.detachEvent("onkeyup", checkInput);
         }
         return false;
     }
-   // console.log(httpRequest);
     return httpRequest;
 }
 
@@ -53,11 +73,36 @@ function getLocation() {
     }
     
     httpRequest.abort();
-    alert(zip);
-    httpRequest.open("get", "http://api.zippopotam.us/us/" + zip,true);
-    alert("2");
+    //var url = "http://api.zippopotam.us/us/" + zip;
+    //httpRequest.open("get", url, true);
+    httpRequest.open("get", "http://api.zippopotam.us/" + countrySel + "/" + zip, true);
     httpRequest.send(null);
-    alert("3");
+    httpRequest.onreadystatechange = displayData;
+}
+
+//Function displayData to get data back
+function displayData() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var resultData = JSON.parse(httpRequest.responseText);
+        var city = document.getElementById("city");
+        var state = document.getElementById("state");
+        city.value = resultData.places[0]["place name"];
+        state.value = resultData.places[0]["state abbreviation"];
+        document.getElementById("zip").blur();
+        document.getElementById("csset").style.visibility = "visible";
+    }
+}
+
+//Event listener for the checkButtons click event
+var germany = document.getElementById("germany");
+var us = document.getElementById("us");
+if (us.addEventListener) {
+    germany.addEventListener("click", checkButtons, false);
+    us.addEventListener("click", checkButtons, false);
+}
+else if (us.attachEvent) {
+    germany.attachEvent("onclick", checkButtons);
+    us.attachEvent("onclick", checkButtons);
 }
 
 //Event listener keyup to fire when user hits keyboard key
